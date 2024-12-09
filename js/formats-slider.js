0ed07d09fdc25f82bcd1c1b6d5e2c95deaac15f7
MilaPalchenko <lancelaps@gmail.com>
2024-12-09 15:04:20 +0200

    fixed slider + pagination

---
+++ js/formats-slider.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,
+        },
+    },
+    // If we need pagination
+    pagination: {
+      el: '.pagination',
+      bulletClass: 'pagination__btn',
+      bulletActiveClass: 'pagination__btn--active',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.carousel-btn.right',
+      prevEl: '.carousel-btn.left',
+    },
+  
+  });
