b6fe239519db42fa04edb6d71ba147974db3abaf
Mila Palchenko <lancelaps@gmail.com>
2025-01-14 19:25:23 +0200

    small fixes, added js for close menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,3 +1,5 @@
+//closing menu on btn click
+
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
@@ -8,3 +10,10 @@ const toggleMenu = () => {
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+
+
+// auto-close mobile menu on click 
+const menuLinks = document.querySelector('.mobile__links-group');
+
+menuLinks.addEventListener('click', toggleMenu);
